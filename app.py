1c30266505668455ac6f7671a527644b6048fc20
eli-jaffe <60531996+eli-jaffe@users.noreply.github.com>
2020-11-23 09:57:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def try_trades():
         for i in positions:
             curr = float(i['current_price'])
             buy_price = float(i['avg_entry_price'])
-            print("Stock: " + i['symbol'] + ", Current price: " + str(curr) + ", Buy price: " + str(buy_price))
+            print("Stock: " + i['symbol'] + ", Current price: " + str(curr) + ", Buy price: " + str(buy_price) + ", Percent change: " + str(curr/buy_price*100))
             if (curr/(1 + TARGET_PERCENT) >= buy_price):
                 response = create_order(i['symbol'],i['qyt'],"sell",'market','gtc')
                 print("Order created!")
