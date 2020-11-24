a05c75772ce79eceebc98a6d90b3d55bafe27b7b
eli-jaffe <60531996+eli-jaffe@users.noreply.github.com>
2020-11-23 20:56:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
 ORDERS_URL = "{}/v2/orders".format(BASE_URL)
 HEADERS = {'APCA-API-KEY-ID': os.environ.get('APCA_API_KEY_ID'), 'APCA-API-SECRET-KEY': os.environ.get('APCA_API_SECRET_KEY')}
 
-# change this depending on your target. Input target percent increase at which to trade (0.XX format)
-TARGET_PERCENT = 0.10
+# A list of stock tickers that are meant for long term gains and should be exluded from percent gain based script
+LONG_TERM = ['ABNB']
+
+# change this depending on your targetpercent
+TARGET_PERCENT = 0.05
 
 def get_account():
     r = requests.get(ACCOUNT_URL, headers=HEADERS)
@@ -39,7 +42,6 @@ def get_positions():
 
     return json.loads(r.content)
 
-print()
 #print(orders)
 #time.sleep(15)
 #response = get_account()
@@ -55,10 +57,14 @@ print()
 # s.login(MY_ADDRESS, PASSWORD)
 ###
 
+
 def try_trades():
     positions = get_positions()
     if positions:
         for i in positions:
+            # exclude long term plays
+            if i['symbol'] in LONG_TERM:
+                continue
             curr = float(i['current_price'])
             buy_price = float(i['avg_entry_price'])
             print("Stock: " + i['symbol'] + ", Current price: " + str(curr) + ", Buy price: " + str(buy_price) + ", Percent change: " + str(curr/buy_price*100))
@@ -66,7 +72,7 @@ def try_trades():
                 response = create_order(i['symbol'],i['qty'],"sell",'market','day')
                 print("Order created to sell " + response['qty'] + " stocks of " + response['symbol'])
 
-                #send email notification (alpaca might do this automatically)
+                #send email notification (alpaca might do this automatically)"""
     else:
         print("You do not currently hold any positions")
 
